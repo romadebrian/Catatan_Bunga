e019bac1c4eb0bca057dece2c3b5d9cee71998ff
romadebrian <roma_coll04@yahoo.com>
2021-03-06 07:24:38 +0700

    Initial commit

---
+++ app/src/main/java/com/roma/catatanbunga/Db/DatabaseContract.java
@@ -0,0 +1,20 @@
+package com.roma.catatanbunga.Db;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    static String TABLE_NOTE = "note";
+
+    static final class NoteColumns implements BaseColumns {
+
+        //Note title
+        static String TITLE = "title";
+        //Note description
+        static String DESCRIPTION = "description";
+        //Note date
+        static String DATE = "date";
+
+    }
+
+}
